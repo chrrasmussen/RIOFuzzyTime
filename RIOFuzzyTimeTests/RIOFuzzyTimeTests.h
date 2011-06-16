109408e4a9688556980bf384b20eae95d3d91f8c
Christian Rasmussen <christian.rasmussen@me.com>
2011-06-16 03:14:44 +0200

    Initial Commit

---
+++ RIOFuzzyTimeTests/RIOFuzzyTimeTests.h
@@ -0,0 +1,13 @@
+//
+//  RIOFuzzyTimeTests.h
+//  RIOFuzzyTimeTests
+//
+//  Created by Christian Rasmussen on 16.06.11.
+//  Copyright 2011 Rasmussen I/O. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RIOFuzzyTimeTests : SenTestCase
+
+@end
---
+++ RIOFuzzyTimeTests/RIOFuzzyTimeTests.m
@@ -0,0 +1,32 @@
+//
+//  RIOFuzzyTimeTests.m
+//  RIOFuzzyTimeTests
+//
+//  Created by Christian Rasmussen on 16.06.11.
+//  Copyright 2011 Rasmussen I/O. All rights reserved.
+//
+
+#import "RIOFuzzyTimeTests.h"
+
+@implementation RIOFuzzyTimeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RIOFuzzyTimeTests");
+}
+
+@end
